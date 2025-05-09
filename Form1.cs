880fde030c8a1e8f363e17a103190843f84bca9b
baokh <baokh@ARUS>
2025-05-09 14:44:21 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+namespace QuanLyTiemTapHoa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+
+        }
+    }
+}
