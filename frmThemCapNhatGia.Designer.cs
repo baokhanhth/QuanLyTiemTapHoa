69a134e327ead5bea988b3e14e468625ed0b2f38
Arus <baokhanhtranhoang726@gmail.com>
2025-05-29 23:09:44 +0700

    Thêm cập nhật giá

---
+++ frmThemCapNhatGia.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace QuanLyTiemTapHoa
+{
+    partial class frmThemCapNhatGia
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        // Thay ToolTip mặc định thành Guna2HtmlToolTip
+        private Guna.UI2.WinForms.Guna2HtmlToolTip tMaHang;
+        private Guna.UI2.WinForms.Guna2HtmlToolTip tMaLo;
+        private Guna.UI2.WinForms.Guna2HtmlToolTip tNgayBD;
+        private Guna.UI2.WinForms.Guna2HtmlToolTip tNgayKT;
+        private Guna.UI2.WinForms.Guna2HtmlToolTip tDonGN;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges17 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges18 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges19 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmThemCapNhatGia));
+            pnlBackground = new Guna.UI2.WinForms.Guna2GradientPanel();
+            tbtnThem = new Guna.UI2.WinForms.Guna2GradientTileButton();
+            txtDonGiaNhap = new Guna.UI2.WinForms.Guna2TextBox();
+            btnThemClose = new Guna.UI2.WinForms.Guna2Button();
+            txtNgayBD = new Guna.UI2.WinForms.Guna2TextBox();
+            txtNgayKT = new Guna.UI2.WinForms.Guna2TextBox();
+            txtMaLo = new Guna.UI2.WinForms.Guna2TextBox();
+            txtMaHang = new Guna.UI2.WinForms.Guna2TextBox();
+            label1 = new Label();
+            btnClose = new Guna.UI2.WinForms.Guna2Button();
+            cpbThem = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            tMaHang = new Guna.UI2.WinForms.Guna2HtmlToolTip();
+            tMaLo = new Guna.UI2.WinForms.Guna2HtmlToolTip();
+            tNgayBD = new Guna.UI2.WinForms.Guna2HtmlToolTip();
+            tNgayKT = new Guna.UI2.WinForms.Guna2HtmlToolTip();
+            tDonGN = new Guna.UI2.WinForms.Guna2HtmlToolTip();
+            pnlBackground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)cpbThem).BeginInit();
+            SuspendLayout();
+            // 
+            // pnlBackground
+            // 
+            pnlBackground.BackColor = Color.Transparent;
+            pnlBackground.BorderRadius = 20;
+            pnlBackground.Controls.Add(tbtnThem);
+            pnlBackground.Controls.Add(txtDonGiaNhap);
+            pnlBackground.Controls.Add(btnThemClose);
+            pnlBackground.Controls.Add(txtNgayBD);
+            pnlBackground.Controls.Add(txtNgayKT);
+            pnlBackground.Controls.Add(txtMaLo);
+            pnlBackground.Controls.Add(txtMaHang);
+            pnlBackground.Controls.Add(label1);
+            pnlBackground.Controls.Add(btnClose);
+            pnlBackground.CustomizableEdges = customizableEdges17;
+            pnlBackground.Dock = DockStyle.Fill;
+            pnlBackground.FillColor = Color.Plum;
+            pnlBackground.FillColor2 = Color.PowderBlue;
+            pnlBackground.Location = new Point(0, 0);
+            pnlBackground.Name = "pnlBackground";
+            pnlBackground.ShadowDecoration.CustomizableEdges = customizableEdges18;
+            pnlBackground.Size = new Size(482, 553);
+            pnlBackground.TabIndex = 1;
+            // 
+            // tbtnThem
+            // 
+            tbtnThem.BackColor = Color.Transparent;
+            tbtnThem.BorderColor = Color.Transparent;
+            tbtnThem.BorderRadius = 18;
+            tbtnThem.CustomizableEdges = customizableEdges1;
+            tbtnThem.DisabledState.BorderColor = Color.DarkGray;
+            tbtnThem.DisabledState.CustomBorderColor = Color.DarkGray;
+            tbtnThem.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            tbtnThem.DisabledState.FillColor2 = Color.FromArgb(169, 169, 169);
+            tbtnThem.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            tbtnThem.FillColor = Color.White;
+            tbtnThem.FillColor2 = Color.White;
+            tbtnThem.Font = new Font("Open Sans ExtraBold", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            tbtnThem.ForeColor = Color.FromArgb(28, 76, 119);
+            tbtnThem.Image = Properties.Resources.plus;
+            tbtnThem.ImageAlign = HorizontalAlignment.Left;
+            tbtnThem.ImageOffset = new Point(5, 28);
+            tbtnThem.ImageSize = new Size(44, 44);
+            tbtnThem.ImeMode = ImeMode.NoControl;
+            tbtnThem.Location = new Point(130, 471);
+            tbtnThem.Name = "tbtnThem";
+            tbtnThem.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            tbtnThem.Size = new Size(191, 50);
+            tbtnThem.TabIndex = 27;
+            tbtnThem.Text = "Thêm";
+            tbtnThem.TextOffset = new Point(40, -30);
+            tbtnThem.Click += tbtnThem_Click;
+            // 
+            // txtDonGiaNhap
+            // 
+            txtDonGiaNhap.BorderRadius = 10;
+            txtDonGiaNhap.CustomizableEdges = customizableEdges3;
+            txtDonGiaNhap.DefaultText = "";
+            txtDonGiaNhap.Font = new Font("Open Sans SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtDonGiaNhap.ForeColor = Color.Black;
+            txtDonGiaNhap.Location = new Point(64, 387);
+            txtDonGiaNhap.Margin = new Padding(3, 4, 3, 4);
+            txtDonGiaNhap.Name = "txtDonGiaNhap";
+            txtDonGiaNhap.PlaceholderForeColor = Color.Black;
+            txtDonGiaNhap.PlaceholderText = "Đơn Giá Nhập Của Hàng Hóa";
+            txtDonGiaNhap.SelectedText = "";
+            txtDonGiaNhap.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            txtDonGiaNhap.Size = new Size(350, 40);
+            txtDonGiaNhap.TabIndex = 11;
+            tDonGN.SetToolTip(txtDonGiaNhap, "<div style='text-align:center;'>Nhập đơn giá nhập của hàng hóa tại đây</div>");
+            // 
+            // btnThemClose
+            // 
+            btnThemClose.BorderRadius = 12;
+            btnThemClose.CustomizableEdges = customizableEdges5;
+            btnThemClose.FillColor = Color.Transparent;
+            btnThemClose.Font = new Font("Segoe UI", 9F);
+            btnThemClose.ForeColor = Color.White;
+            btnThemClose.Image = Properties.Resources.cancel_50px;
+            btnThemClose.ImageSize = new Size(50, 50);
+            btnThemClose.Location = new Point(420, 4);
+            btnThemClose.Name = "btnThemClose";
+            btnThemClose.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            btnThemClose.Size = new Size(50, 50);
+            btnThemClose.TabIndex = 6;
+            btnThemClose.Click += btnClose_Click;
+            // 
+            // txtNgayBD
+            // 
+            txtNgayBD.BorderRadius = 10;
+            txtNgayBD.CustomizableEdges = customizableEdges7;
+            txtNgayBD.DefaultText = "";
+            txtNgayBD.Font = new Font("Open Sans SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtNgayBD.ForeColor = Color.Black;
+            txtNgayBD.Location = new Point(64, 248);
+            txtNgayBD.Margin = new Padding(3, 4, 3, 4);
+            txtNgayBD.Multiline = true;
+            txtNgayBD.Name = "txtNgayBD";
+            txtNgayBD.PlaceholderForeColor = Color.Black;
+            txtNgayBD.PlaceholderText = "Ngày Bắt Đầu Đơn Giá";
+            txtNgayBD.SelectedText = "";
+            txtNgayBD.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            txtNgayBD.Size = new Size(350, 40);
+            txtNgayBD.TabIndex = 0;
+            txtNgayBD.TextOffset = new Point(0, -4);
+            tNgayBD.SetToolTip(txtNgayBD, "<div style='text-align:center;'>Nhập ngày bắt đầu đơn giá tại đây</div>");
+            // 
+            // txtNgayKT
+            // 
+            txtNgayKT.BorderRadius = 10;
+            txtNgayKT.CustomizableEdges = customizableEdges9;
+            txtNgayKT.DefaultText = "";
+            txtNgayKT.Font = new Font("Open Sans SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtNgayKT.ForeColor = Color.Black;
+            txtNgayKT.Location = new Point(64, 316);
+            txtNgayKT.Margin = new Padding(3, 4, 3, 4);
+            txtNgayKT.Name = "txtNgayKT";
+            txtNgayKT.PlaceholderForeColor = Color.Black;
+            txtNgayKT.PlaceholderText = "Ngày Kết Thúc Đơn Giá";
+            txtNgayKT.SelectedText = "";
+            txtNgayKT.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            txtNgayKT.Size = new Size(350, 40);
+            txtNgayKT.TabIndex = 1;
+            tNgayKT.SetToolTip(txtNgayKT, "<div style='text-align:center;'>Nhập ngày kết thúc đơn giá tại đây</div>");
+            // 
+            // txtMaLo
+            // 
+            txtMaLo.BorderRadius = 10;
+            txtMaLo.CustomizableEdges = customizableEdges11;
+            txtMaLo.DefaultText = "";
+            txtMaLo.Font = new Font("Open Sans SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtMaLo.ForeColor = Color.Black;
+            txtMaLo.Location = new Point(64, 174);
+            txtMaLo.Margin = new Padding(3, 4, 3, 4);
+            txtMaLo.Name = "txtMaLo";
+            txtMaLo.PlaceholderForeColor = Color.Black;
+            txtMaLo.PlaceholderText = "Mã Lô Hàng Hóa";
+            txtMaLo.SelectedText = "";
+            txtMaLo.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            txtMaLo.Size = new Size(350, 40);
+            txtMaLo.TabIndex = 4;
+            tMaLo.SetToolTip(txtMaLo, "<div style='text-align:center;'>Nhập mã lô hàng hóa tại đây</div>");
+            // 
+            // txtMaHang
+            // 
+            txtMaHang.BorderRadius = 10;
+            txtMaHang.CustomizableEdges = customizableEdges13;
+            txtMaHang.DefaultText = "";
+            txtMaHang.Font = new Font("Open Sans SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtMaHang.ForeColor = Color.Black;
+            txtMaHang.Location = new Point(64, 105);
+            txtMaHang.Margin = new Padding(3, 4, 3, 4);
+            txtMaHang.Name = "txtMaHang";
+            txtMaHang.PlaceholderForeColor = Color.Black;
+            txtMaHang.PlaceholderText = "Mã Hàng Hóa";
+            txtMaHang.SelectedText = "";
+            txtMaHang.ShadowDecoration.CustomizableEdges = customizableEdges14;
+            txtMaHang.Size = new Size(350, 40);
+            txtMaHang.TabIndex = 3;
+            tMaHang.SetToolTip(txtMaHang, "<div style='text-align:center;'>Nhập mã hàng hóa tại đây</div>");
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Open Sans ExtraBold", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(27, 20, 100);
+            label1.Location = new Point(118, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(219, 40);
+            label1.TabIndex = 5;
+            label1.Text = "Thêm Đơn Giá";
+            // 
+            // btnClose
+            // 
+            btnClose.BorderRadius = 10;
+            btnClose.CustomizableEdges = customizableEdges15;
+            btnClose.DisabledState.BorderColor = Color.DarkGray;
+            btnClose.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnClose.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnClose.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnClose.FillColor = Color.Transparent;
+            btnClose.Font = new Font("Segoe UI", 9F);
+            btnClose.ForeColor = Color.White;
+            btnClose.Image = Properties.Resources.cancel_50px;
+            btnClose.ImageSize = new Size(50, 50);
+            btnClose.Location = new Point(420, 4);
+            btnClose.Name = "btnClose";
+            btnClose.ShadowDecoration.CustomizableEdges = customizableEdges16;
+            btnClose.Size = new Size(50, 50);
+            btnClose.TabIndex = 7;
+            btnClose.Click += btnClose_Click;
+            // 
+            // cpbThem
+            // 
+            cpbThem.BackColor = Color.Transparent;
+            cpbThem.FillColor = Color.Transparent;
+            cpbThem.ImageRotate = 0F;
+            cpbThem.Location = new Point(194, 41);
+            cpbThem.Name = "cpbThem";
+            cpbThem.ShadowDecoration.CustomizableEdges = customizableEdges19;
+            cpbThem.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            cpbThem.Size = new Size(100, 100);
+            cpbThem.SizeMode = PictureBoxSizeMode.StretchImage;
+            cpbThem.TabIndex = 29;
+            cpbThem.TabStop = false;
+            // 
+            // tMaHang
+            // 
+            tMaHang.AllowLinksHandling = true;
+            tMaHang.MaximumSize = new Size(0, 0);
+            // 
+            // tMaLo
+            // 
+            tMaLo.AllowLinksHandling = true;
+            tMaLo.MaximumSize = new Size(0, 0);
+            // 
+            // tNgayBD
+            // 
+            tNgayBD.AllowLinksHandling = true;
+            tNgayBD.MaximumSize = new Size(0, 0);
+            // 
+            // tNgayKT
+            // 
+            tNgayKT.AllowLinksHandling = true;
+            tNgayKT.MaximumSize = new Size(0, 0);
+            // 
+            // tDonGN
+            // 
+            tDonGN.AllowLinksHandling = true;
+            tDonGN.MaximumSize = new Size(0, 0);
+            // 
+            // frmThemCapNhatGia
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(482, 553);
+            Controls.Add(pnlBackground);
+            Controls.Add(cpbThem);
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "frmThemCapNhatGia";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "frmThemCapNhatGia";
+            pnlBackground.ResumeLayout(false);
+            pnlBackground.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)cpbThem).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2GradientPanel pnlBackground;
+        private Guna.UI2.WinForms.Guna2GradientTileButton tbtnThem;
+        private Guna.UI2.WinForms.Guna2TextBox txtDonGiaNhap;
+        private Guna.UI2.WinForms.Guna2Button btnThemClose;
+        private Guna.UI2.WinForms.Guna2TextBox txtNgayBD;
+        private Guna.UI2.WinForms.Guna2TextBox txtNgayKT;
+        private Guna.UI2.WinForms.Guna2TextBox txtMaLo;
+        private Guna.UI2.WinForms.Guna2TextBox txtMaHang;
+        private Label label1;
+        private Guna.UI2.WinForms.Guna2Button btnClose;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox cpbThem;
+    }
+}
