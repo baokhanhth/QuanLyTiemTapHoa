f678a4c3c6a8a06b096fa1338338e9622ae4bdd5
Arus <baokhanhtranhoang726@gmail.com>
2025-05-29 21:10:08 +0700

    thêm cấu hình database cho phần mềm

---
+++ Helpers/DBconfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace QuanLyTiemTapHoa.Helpers
+{
+    /// <summary>
+    /// Helper class cung cấp chung connection string cho toàn ứng dụng.
+    /// </summary>
+    public static class DbConfig
+    {
+        /// <summary>
+        /// Lấy connection string từ app.config (hoặc web.config).
+        /// </summary>
+        public static string ConnectionString =>
+            ConfigurationManager
+                .ConnectionStrings["QuanLyBanHang"]
+                .ConnectionString;
+    }
+}
